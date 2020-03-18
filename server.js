a32ac9197dd3edbfe7d92e86efacabe15ca006e7
Christian Sanchez <cas81695@gmail.com>
2020-03-18 16:03:19 -0400

    edits

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-require("./routes/apiRoutes")(app);
+require("./Develop/routes/apiRoutes")(app);
 
-require("./routes/htmlRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
